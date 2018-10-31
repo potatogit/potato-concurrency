6c84f027f1f4d83d629489f87fdef00038a82628
yang_yu <yang_yu@worksap.co.jp>
2018-10-31 19:26:06 +0800

    Add concurrency container demo.

---
+++ src/main/java/com/potato/concurrency/concurrency/CopyOnWriteArrayLIstExample.java
@@ -0,0 +1,50 @@
+package com.potato.concurrency.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.potato.concurrency.annotation.NotThreadSafe;
+
+@Slf4j
+@NotThreadSafe
+public class CopyOnWriteArrayLIstExample {
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    private static List<Integer> list = new CopyOnWriteArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal ; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    append();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("length:{}", list.size());
+    }
+
+    private static void append() {
+        list.add(1);
+    }
+}
